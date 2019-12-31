e57007bf3162ef8298ce50e479d7fed31096d01b
Kishan Jasani <kishanjasani007@yahoo.in>
2019-12-31 18:01:45 +0530

    Add search component Pass data from child to parent

--- fetch-images/src/components/Search.js
+++ fetch-images/src/components/Search.js
@@ -2,23 +2,24 @@ import React from 'react';
 
 class Search extends React.Component {
 
-  onInputChange = event => {
-    console.log(event.target.value);
+  state = { term: '' }
 
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
   }
 
   onSubmit = event => {
     event.preventDefault();
-    console.log('Submited');
+    this.props.onSubmit(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onClick={this.onInputClick} onSubmit={this.onSubmit} onChange={this.onInputChange} />
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
         </form>
       </div>
